113eb2589d39a37407e94805e0ae3407b113ab06
Giga Ananidze <ananidzegiga@gmail.com>
2024-06-15 00:14:22 +0400

    commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2414680366"
+    serviceWorkerVersion: "3648983464"
   }
 });
